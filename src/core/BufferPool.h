89063391374fa0bee030c34c4ea1ecce4c79c321
Bill Liu <yuan6liu@gmail.com>
2014-09-27 15:11:18 -0700

    modified BufferPool to take in Template Buffer container type

--- src/core/BufferPool.h
+++ src/core/BufferPool.h
@@ -1,12 +1,12 @@
 #ifndef BUFFER_POOL_H
 #define BUFFER_POOL_H
 
-#include "CircularBufferThreadSafe.h"
+// #include "CircularBufferThreadSafe.h"
 
 #include <vector>
 using namespace std;
 
-template< typename T >
+template< typename Buffer, typename T >
 class BufferPool{
 public:
                                              BufferPool();
@@ -14,55 +14,54 @@ public:
   void                                       SetNumBuffers( int num ); // -1 removed all buffers
   int                                        GetNumBuffers() const;
   bool                                       AddToBuffers( T & a );
-  bool                                       GetBufferAtIndex(int index, CircularBufferThreadSafe< T > * & bufptr ) const;
+  bool                                       GetBufferAtIndex(int index, Buffer * & bufptr ) const;
+  void                                       ResetIndex();
+  bool                                       ConsumeBuffersRandom( T & a );
 private:
-  vector< CircularBufferThreadSafe< T > * >  _buffers; //vector of buffers
+  vector< Buffer * >  _buffers; //vector of buffers
   int                                        _bufSelIndex;
+  int                                        _bufConsumeIndex;
 };
 
-template< typename T >
-BufferPool< T > :: BufferPool(){
+template< typename Buffer, typename T >
+BufferPool< Buffer, T > :: BufferPool(){
   SetNumBuffers(1);
-  _bufSelIndex = -1;
 }
 
-template< typename T >
-void BufferPool< T > :: Clear(){
+template< typename Buffer, typename T >
+void BufferPool< Buffer, T > :: Clear(){
   SetNumBuffers(-1);
 }
 
-template< typename T >
-void BufferPool< T > :: SetNumBuffers( int num ){
+template< typename Buffer, typename T >
+void BufferPool< Buffer, T > :: SetNumBuffers( int num ){
   if( num <= 0 ){
     _buffers.clear();
-    _bufSelIndex = -1;
-    return;
   }else{
     int diff = num - _buffers.size();
     if( diff > 0 ){
       while( diff > 0 ){
-	CircularBufferThreadSafe< T > * cb = new CircularBufferThreadSafe< T >;
+	Buffer * cb = new Buffer;
 	_buffers.push_back( cb );
 	--diff;
       }
-      _bufSelIndex = 0;
     }else if( diff < 0 ){
       while( diff < 0 ){
 	_buffers.pop_back();
 	++diff;
       }
-      _bufSelIndex = 0;
     }
   }
+  ResetIndex();
 }
 
-template< typename T >
-int BufferPool< T > :: GetNumBuffers() const{
+template< typename Buffer, typename T >
+int BufferPool< Buffer, T > :: GetNumBuffers() const{
   return _buffers.size();
 }
 
-template< typename T >
-bool BufferPool< T > :: AddToBuffers( T & a ){
+template< typename Buffer, typename T >
+bool BufferPool< Buffer, T > :: AddToBuffers( T & a ){
   if( _bufSelIndex == -1 ){
     return false;
   }else if( _bufSelIndex >=  _buffers.size()){
@@ -81,8 +80,8 @@ bool BufferPool< T > :: AddToBuffers( T & a ){
   return false;
 }
 
-template< typename T >
-bool BufferPool< T > :: GetBufferAtIndex(int index, CircularBufferThreadSafe< T > * & bufptr ) const{
+template< typename Buffer, typename T >
+bool BufferPool< Buffer, T > :: GetBufferAtIndex(int index, Buffer * & bufptr ) const{
   if( _buffers.empty() == false && index >= 0 && index < _buffers.size() ){
     bufptr = _buffers.at( index );
     return true;
@@ -90,6 +89,36 @@ bool BufferPool< T > :: GetBufferAtIndex(int index, CircularBufferThreadSafe< T 
     bufptr = nullptr;
     return false;
   }
+}
+
+template< typename Buffer, typename T >
+void BufferPool< Buffer, T > :: ResetIndex(){
+    if( _buffers.empty() ){
+        _bufSelIndex = -1;
+        _bufConsumeIndex = -1;
+    }else{
+        _bufSelIndex = 0;
+        _bufConsumeIndex = 0;
+    }
+}
+
+template< typename Buffer, typename T >
+bool BufferPool< Buffer, T > :: ConsumeBuffersRandom( T & a ){
+  if( _bufConsumeIndex == -1 ){
+    return false;
+  }else if( _bufConsumeIndex >=  _buffers.size()){
+    _bufConsumeIndex = 0;
+  }
+
+  bool ret = false;
+  while( ret == false && _bufConsumeIndex < _buffers.size() ){
+    ret = ( _buffers.at(_bufConsumeIndex) )->Consume( a );
+    ++_bufConsumeIndex;
+    if( ret == true ){
+      return true;
+    }
+  }
+  return false;
 }
 
 #endif
--- test/core/test_BufferPool.cpp
+++ test/core/test_BufferPool.cpp
@@ -9,84 +9,143 @@ using namespace std;
 
 TEST_CASE( "ThreadPool", "[ThreadPool]" ) {
   
-  BufferPool< string > tpool;
+    BufferPool< CircularBufferThreadSafe<string>, string > tpool;
 
-  SECTION( "Set number of buffers" ) {
-    REQUIRE( tpool.GetNumBuffers() == 1 ); // default is 1
-    tpool.SetNumBuffers(3);
-    REQUIRE( tpool.GetNumBuffers() == 3 );
-    tpool.SetNumBuffers(2);
-    REQUIRE( tpool.GetNumBuffers() == 2 );
-  }
-  SECTION( "Get buffer at index" ) {
-    CircularBufferThreadSafe< string > * buf = NULL;
-    tpool.SetNumBuffers(3);
-    bool ret;
-    for( int i = 0; i < 3; i++ ){
-      ret = tpool.GetBufferAtIndex( i, buf );
-      REQUIRE( ret == true );
-      REQUIRE( buf != NULL );
+    SECTION( "Set number of buffers" ) {
+        CHECK( tpool.GetNumBuffers() == 1 ); // default is 1
+        tpool.SetNumBuffers(3);
+        CHECK( tpool.GetNumBuffers() == 3 );
+        tpool.SetNumBuffers(2);
+        CHECK( tpool.GetNumBuffers() == 2 );
     }
-    ret = tpool.GetBufferAtIndex( -1, buf );
-    REQUIRE( ret == false );
-    REQUIRE( buf == NULL );
-    ret = tpool.GetBufferAtIndex( 3, buf );
-    REQUIRE( ret == false );
-    REQUIRE( buf == NULL );
-  }
-  SECTION( "Add To Buffers, Clear" ) {
-    CircularBufferThreadSafe< string > * buf = NULL;
-    tpool.SetNumBuffers(3);
-    string str = "quartet";
-    tpool.AddToBuffers( str );
-    string str2 = "voyageur";
-    tpool.AddToBuffers( str2 );
+    SECTION( "Get buffer at index" ) {
+        CircularBufferThreadSafe< string > * buf = NULL;
+        tpool.SetNumBuffers(3);
+        bool ret;
+        for( int i = 0; i < 3; i++ ){
+            ret = tpool.GetBufferAtIndex( i, buf );
+            CHECK( ret == true );
+            CHECK( buf != NULL );
+        }
+        ret = tpool.GetBufferAtIndex( -1, buf );
+        CHECK( ret == false );
+        CHECK( buf == NULL );
+        ret = tpool.GetBufferAtIndex( 3, buf );
+        CHECK( ret == false );
+        CHECK( buf == NULL );
+    }
+    SECTION( "Add and Consume Target Buffers" ) {
+        CircularBufferThreadSafe< string > * buf = NULL;
+        tpool.SetNumBuffers(3);
+        string str = "quartet";
+        string temp = string(str);
+        tpool.AddToBuffers( temp );
+        string str2 = "voyageur";
+        temp = string(str2);
+        tpool.AddToBuffers( temp );
 
-    int getcount = 0;
-    for( int i = 0; i < 3; i++ ){
-      tpool.GetBufferAtIndex( i, buf );
-      string getstr;
-      bool ret = buf->Consume(getstr);
-      if(ret == true){
-	REQUIRE( i <= 1 ); // test to make sure only first two buffer got the data
-	if( i == 0 ){
-	  REQUIRE( getstr == str );
-	}else if( i == 1 ){
-	  REQUIRE( getstr == str2 );
-	}
-	++getcount;
-      }
-    }
-    REQUIRE( getcount == 2 );	
+        int getcount = 0;
+        for( int i = 0; i < 3; i++ ){
+            bool ret = tpool.GetBufferAtIndex( i, buf );
+            REQUIRE( true == ret );
+            string getstr;
+            ret = buf->Consume(getstr);
+            if( i == 2 ){
+                CHECK( false == ret );
+            }else{
+                CHECK( true == ret );
+            }
+            if(ret == true){
+                REQUIRE( i <= 1 ); // test to make sure only first two buffer got the data
+                if( i == 0 ){
+                    REQUIRE( getstr == str );
+                }else if( i == 1 ){
+                    REQUIRE( getstr == str2 );
+                }
+                ++getcount;
+            }
+        }
+        REQUIRE( getcount == 2 );	
 
-    //reset internal buffer index counter for tests below
-    tpool.Clear();
-    tpool.SetNumBuffers(3);
+        //reset internal buffer index counter for tests below
+        tpool.Clear();
+        tpool.SetNumBuffers(3);
 
-    //try add beyond some set limit
-    for( int i = 0; i < 3; i++ ){
-      tpool.GetBufferAtIndex( i, buf );
-      buf->SetMaxSize(1); // make size of 1 for each buffer
+        //try add beyond some set limit
+        for( int i = 0; i < 3; i++ ){
+            tpool.GetBufferAtIndex( i, buf );
+            buf->SetMaxSize(1); // make size of 1 for each buffer
+        }
+        for( int i = 0; i < 4; i++ ){ // add 4 items to 3 buffers
+            stringstream ss;
+            ss << str << i;
+            string str_temp;
+            ss >> str_temp;
+            bool ret = tpool.AddToBuffers( str_temp );
+            if( i == 3 ){
+                CHECK( false == ret );
+            }
+            else{
+                CHECK( true == ret );
+            }
+        }    
+        for( int i = 0; i < 3; i++ ){ //check to make sure 4th item is not added
+            bool ret = tpool.GetBufferAtIndex( i, buf );
+            CHECK( true == ret );      
+            string getstr;
+            ret = buf->Consume(getstr);
+            CHECK( true == ret );
+            stringstream ss;
+            ss << str << i;
+            string str_temp;
+            ss >> str_temp;
+            CHECK( getstr == str_temp );
+            CHECK( buf->GetSize() == 0 );
+        }
     }
-    for( int i = 0; i < 4; i++ ){ // add 4 items to 3 buffers
-      stringstream ss;
-      ss << str << i;
-      string str_temp;
-      ss >> str_temp;
-      tpool.AddToBuffers( str_temp );
-    }    
-    for( int i = 0; i < 3; i++ ){ //check too make sure 4th item is not added
-      tpool.GetBufferAtIndex( i, buf );
-      string getstr;
-      bool ret = buf->Consume(getstr);
-      if( ret == true ){
-	stringstream ss;
-	ss << str << i;
-	string str_temp;
-	ss >> str_temp;
-	REQUIRE( getstr == str_temp );
-	REQUIRE( buf->GetSize() == 0 );
-      }
+
+    SECTION( "Consume Random Buffers" ) {
+        CircularBufferThreadSafe< string > * buf = NULL;
+        tpool.SetNumBuffers(3);
+        string str = "quartet";
+        //reset internal buffer index counter for tests below
+        tpool.Clear();
+        tpool.SetNumBuffers(3);
+
+        //try add beyond some set limit
+        for( int i = 0; i < 3; i++ ){
+            bool ret = tpool.GetBufferAtIndex( i, buf );
+            CHECK( true == ret );
+            buf->SetMaxSize(2); 
+        }
+        tpool.ResetIndex();
+        for( int i = 0; i < 4; i++ ){ // add 4 items to 3 buffers
+            stringstream ss;
+            ss << str << i;
+            string str_temp;
+            ss >> str_temp;
+            tpool.AddToBuffers( str_temp );
+        }  
+        tpool.ResetIndex();
+        for( int i = 0; i < 3; i++ ){ // consume random buffers 3 times and check results
+            string getstr;
+            bool ret = tpool.ConsumeBuffersRandom(getstr);
+            CHECK( true == ret );
+            stringstream ss;
+            ss << str << i;
+            string str_temp;
+            ss >> str_temp;
+            CHECK( getstr == str_temp );
+        }
+        tpool.ResetIndex();
+        for( int i = 0; i < 3; i++ ){ //check to make sure there is 1 remaining item in 1st buffer and other buffers are empty
+            bool ret = tpool.GetBufferAtIndex( i, buf );
+            REQUIRE( true == ret );
+            if( i == 0){
+                CHECK( buf->GetSize() == 1 );
+            }else{
+                CHECK( buf->GetSize() == 0 );
+            }
+        }
     }
-  }
 }
