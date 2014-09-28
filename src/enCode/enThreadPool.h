a211262f9df6dec91a93d7dce33c10a9f626a816
Bill Liu <yuan6liu@gmail.com>
2014-09-27 23:51:01 -0700

    added RunThreads(), modified SetNumThreads()

--- src/enCode/enThreadPool.h
+++ src/enCode/enThreadPool.h
@@ -4,20 +4,35 @@
 #include "ThreadPool.h"
 #include <vector>
 
-template< typename BufferType, typename ThreadType, typename TaskType >
+class FuncWrap;
+
+template< typename BufferType, typename ThreadType >
 class enThreadPool : public ThreadPool {
 public:
-    bool GetTask( TaskType & fw ){
+    enThreadPool(){
+        SetNumThreads(1);
+    }
+    bool GetTask( FuncWrap & fw ){
         return _BuffPool.GetFromBuffer( fw );
     }
     void SetNumThreads( int n ){
-
+        _BuffPool.SetNumBuffers( n );
+        _vThread.clear();
+        for( int i = 0; i < n; i++ ){
+            ThreadType * NewThread = new ThreadType;
+            NewThread->SetThreadPool( this );
+            _vThread.push_back( NewThread );
+        }
+    }
+    void RunThreads(){
+        for( int i = 0; i < _vThread.size(); i++ ){
+            _vThread.at(i)->Run();
+        }
     }
 private:
-    // BufferPool< CircularBufferThreadSafe< TaskType >, TaskType >   _BuffPool;
     BufferType                              _BuffPool;
-    std::vector< ThreadType >               _vThread;
-    void AddTaskHook( TaskType & fw ){
+    std::vector< ThreadType * >             _vThread;
+    void AddTaskHook( FuncWrap & fw ){
         _BuffPool.AddToBuffer( fw );
     }
 };
