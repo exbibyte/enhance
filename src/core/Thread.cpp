663fa5b0c574665a2e36f781c90d66c87bc14949
Bill Liu <yuan6liu@gmail.com>
2014-09-28 19:28:45 -0700

    Added thread end, pause, continue, waiting functions and thread state defines

--- src/core/Thread.cpp
+++ src/core/Thread.cpp
@@ -1,16 +1,44 @@
 #ifdef THREAD_H
 
-void Thread::WaitForThread(){
-    while( _access.load(std::memory_order_relaxed) >= 0 );
+void Thread::WaitForThreadEnd(){   /// blocking until end state
+    while( _Access.load(std::memory_order_relaxed) != THREAD_ENDED );
+}
+
+void Thread::WaitForThreadIdle(){   /// blocking until idle state
+    while( _Access.load(std::memory_order_relaxed) != THREAD_IDLE );
+}
+
+void Thread::WaitForThreadNotPause(){   /// blocking until not pausing state
+    while( _Access.load(std::memory_order_relaxed) == THREAD_PAUSED );
 }
 
 bool Thread::Run(){
-  _StdThread = std::thread( &Thread::RunHook, this );
-  _StdThread.detach(); //let the thread loose
-  return true;
+    _Access.store( THREAD_BUSY, std::memory_order_relaxed); /// start thread and flag busy state
+    _StdThread = std::thread( &Thread::RunHook, this );
+    _StdThread.detach(); //let the thread loose
+    return true;
 }
 
 void Thread::RunHook(){
+    _Access.store( THREAD_ENDED, std::memory_order_relaxed); /// default action is end thread
+}
+
+void Thread::PauseThread(){
+    if( _Access.load(std::memory_order_relaxed) != THREAD_PAUSED ){ /// flag pause state if current state is not paused
+        _Access.store( THREAD_NOTIFY_PAUSE, std::memory_order_relaxed);
+    }
+}
+
+void Thread::EndThread(){
+    if( _Access.load(std::memory_order_relaxed) != THREAD_ENDED ){ /// flag end state if current state is not ended
+        _Access.store( THREAD_NOTIFY_END, std::memory_order_relaxed);
+    }
+}
+
+void Thread::ContinueThread(){
+    if( _Access.load(std::memory_order_relaxed) == THREAD_PAUSED ){ /// flag continue state if current state is paused
+        _Access.store( THREAD_NOTIFY_CONTINUE, std::memory_order_relaxed);
+    }
 }
 
 #endif
--- src/core/Thread.h
+++ src/core/Thread.h
@@ -4,18 +4,32 @@
 #include <thread>
 #include <atomic>
 
+#define THREAD_IDLE            -1
+#define THREAD_BUSY             0
+#define THREAD_PAUSED           1
+#define THREAD_ENDED            2
+#define THREAD_NOTIFY_END       3
+#define THREAD_NOTIFY_PAUSE     4
+#define THREAD_NOTIFY_CONTINUE  5
+
 class Thread {
 public:
-                                       Thread():_access(-1) {};
+                                       Thread() : _Access( THREAD_IDLE ) {};
     virtual                            ~Thread(){};
                                        Thread( const Thread &) = delete;
     Thread &                           operator=( const Thread & ) = delete;
-    void                               WaitForThread();
+    void                               WaitForThreadEnd();
+    void                               WaitForThreadIdle();
+    void                               WaitForThreadNotPause();
     bool                               Run();
     virtual void                       RunHook();
+    void                               PauseThread();
+    void                               EndThread();
+    void                               ContinueThread();
+protected:    
+    std::atomic<int>                   _Access; /// thread state indicator
 private:
     std::thread                        _StdThread;
-    std::atomic<int>                   _access; // -1 for idle
 };
 
 #include "Thread.cpp"
