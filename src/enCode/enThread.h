c75f9bf2b2fb928993106cb5ac3a6a3346b475a0
Bill Liu <yuan6liu@gmail.com>
2014-09-28 19:29:45 -0700

    added logic for pausing, ending, and setting current thread state in RunHook()

--- src/enCode/enThread.h
+++ src/enCode/enThread.h
@@ -2,7 +2,6 @@
 #define EN_THREAD_H
 
 #include "Thread.h"
-
 #include "FuncWrap.h"
 
 class ThreadPool;
@@ -12,16 +11,30 @@ public:
     enThread() : _enTP(0) {}
     void SetThreadPool( ThreadPool * enTP ) { _enTP = enTP; }
     void RunHook(){
-        // keep consuming from buffer if buffer is not empty
-        while( _enTP ){ 
+        while( _enTP ){         /// keep consuming from buffer if buffer is not empty
             FuncWrap TaskExecute;
-            bool bRet = _enTP->GetTask( TaskExecute ); // get item from buffer
+            bool bRet = _enTP->GetTask( TaskExecute ); /// get item from buffer
             if( bRet ){
-                TaskExecute(); //execute task
+                _Access.store( THREAD_BUSY, std::memory_order_relaxed);
+                TaskExecute(); /// execute task
             }else{
+                /// else do nothing
+                _Access.store( THREAD_IDLE, std::memory_order_relaxed);
+            }
+
+            /// condition for pause or end
+            if( _Access.load(std::memory_order_relaxed) == THREAD_NOTIFY_END ){
                 break;
             }
+            else if( _Access.load(std::memory_order_relaxed) == THREAD_NOTIFY_PAUSE ){
+                _Access.store( THREAD_PAUSED, std::memory_order_relaxed);
+                WaitForThreadNotPause();
+            }
+            else if( _Access.load(std::memory_order_relaxed) == THREAD_NOTIFY_CONTINUE ){
+                _Access.store( THREAD_BUSY, std::memory_order_relaxed);
+            }
         }
+        _Access.store( THREAD_ENDED, std::memory_order_relaxed); /// end thread
     }
 private:
     ThreadPool * _enTP;
