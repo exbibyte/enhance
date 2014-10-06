960c28a7b45ffbecc47b279179a78b847159734c
Bill Liu <yuan6liu@gmail.com>
2014-10-05 22:42:44 -0700

    experimenting with glsl

--- scratchpad/glsl_hello/ogl.cpp
+++ scratchpad/glsl_hello/ogl.cpp
@@ -7,11 +7,11 @@
 
 #include "GLHelper.h"
 
+#define GLM_FORCE_RADIANS
+
 #include <glm/glm.hpp>
-using glm::mat4;
-using glm::vec3;
-using glm::gtc::matrix_transform;
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 GLuint v,f,f2,p;
 float lpos[4] = {1,0.5,1,0};
@@ -41,40 +41,24 @@ void renderScene(void) {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glLoadIdentity();
-	gluLookAt(0.0,0.0,5.0, 
-		      0.0,0.0,-1.0,
-			  0.0f,1.0f,0.0f);
+	// glLoadIdentity();
+	// gluLookAt(0.0,0.0,5.0, 
+	// 	      0.0,0.0,-1.0,
+	// 		  0.0,1.0,0.0);
 
 	glLightfv(GL_LIGHT0, GL_POSITION, lpos);
 
-        glm::mat4 m_projectionMatrix = glm::perspective(60.0, 1.333, 0.1, 1000.0);
+        // glm::mat4 m_projectionMatrix = glm::perspective(60.0f, 1.333f, 0.1f, 1000.0f);
 
-        glm::mat4 m_modelViewMatrix = glm::lookat(
-            0.0,0.0,10.0, 
-            0.0,0.0,-1.0,
-            0.0f,1.0f,0.0f
-        );
-        // m_modelViewMatrix *= glm::rotate(1,1,1,1);
-        // mat4 rotationMatrix = glm::rotate(mat4(1.0f), angle, vec3(0.0f,0.0f,1.0f));
+        // glm::vec3 eye = glm::vec3(0.0f, 0.0f, 10.0f);
+        // glm::vec3 target = glm::vec3(0.0f, 0.0f, -1.0f);
+        // glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+        // glm::mat4 m_modelViewMatrix = glm::lookAt( eye, target, up );
+        // // m_modelViewMatrix *= glm::rotate(1,1,1,1);
+        // // mat4 rotationMatrix = glm::rotate(mat4(1.0f), angle, vec3(0.0f,0.0f,1.0f));
 
-        GLint loc = glGetUniformLocation(p, "modelview_matrix");
-        double dArray[16] = {0.0};
-        const float *pSource = (const float*)glm::value_ptr(m_modelViewMatrix);
-        for (int i = 0; i < 16; ++i){
-            dArray[i] = pSource[i];
-        }
-        glProgramUniformMatrix4fv(p, loc, 1, GL_FALSE, dArray);
-    
-        loc = glGetUniformLocation(p, "projection_matrix");
-        double dArray2[16] = {0.0};
-        const float *pSource2 = (const float*)glm::value_ptr(_projectionMatrix);
-        for (int i = 0; i < 16; ++i){
-            dArray2[i] = pSource2[i];
-        }
-        glProgramUniformMatrix4fv(p, loc, 1, GL_FALSE, dArray2);
-
-	glutSolidTeapot(1);
+        glBindVertexArray(vaoHandle);
+        glDrawArrays(GL_TRIANGLES, 0, 3 );
 
 	glutSwapBuffers();
 }
@@ -117,12 +101,47 @@ void setShaders() {
 	glAttachShader(p,f);
 	glAttachShader(p,v);
 
-        glGetUniformLocation(p, "projection_matrix");
-        glGetUniformLocation(p, "modelview_matrix");
+        // Bind index 0 to the shader input variable "VertexPosition"
+        glBindAttribLocation(p, 0, "VertexPosition");
+        // Bind index 1 to the shader input variable "VertexColor"
+        glBindAttribLocation(p, 1, "VertexColor");
 
 	GLLinkProgram(p);
-}
 
+        float positionData[] = {
+            -0.8f, -0.8f, 0.0f,
+            0.8f, -0.8f, 0.0f,
+            0.0f, 0.8f, 0.0f };
+
+        float colorData[] = {
+            1.0f, 0.0f, 0.0f, 
+            0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 1.0f };
+
+
+        glGenBuffers(2, vboHandles);
+        GLuint positionBufferHandle = vboHandles[0];
+        GLuint colorBufferHandle = vboHandles[1];
+        // Populate the position buffer
+        glBindBuffer(GL_ARRAY_BUFFER, positionBufferHandle);
+        glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), positionData, GL_STATIC_DRAW);
+        // Populate the color buffer
+        glBindBuffer(GL_ARRAY_BUFFER, colorBufferHandle);
+        glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), colorData, GL_STATIC_DRAW);
+
+        // Create and set-up the vertex array object
+        glGenVertexArrays( 1, &vaoHandle );
+        glBindVertexArray(vaoHandle);
+        // Enable the vertex attribute arrays
+        glEnableVertexAttribArray(0); // Vertex position
+        glEnableVertexAttribArray(1); // Vertex color
+        // Map index 0 to the position buffer
+        glBindBuffer(GL_ARRAY_BUFFER, positionBufferHandle);
+        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL );
+        // Map index 1 to the color buffer
+        glBindBuffer(GL_ARRAY_BUFFER, colorBufferHandle);
+        glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL );
+}
 
 int main(int argc, char **argv) {
 	glutInit(&argc, argv);
--- scratchpad/glsl_hello/toon.vert
+++ scratchpad/glsl_hello/toon.vert
@@ -1,18 +1,9 @@
-// simple toon vertex shader
-// www.lighthouse3d.com
-
-varying vec3 normal, lightDir;
-
-uniform mat4 projection_matrix;
-uniform	mat4 modelview_matrix;
- 
-varying vec3 vertex;
+in vec3 VertexPosition;
+in vec3 VertexColor;
+varying vec3 Color;
 
 void main()
-{	
-           
-     	lightDir = normalize(vec3(gl_LightSource[0].position));
-        normal = normalize(gl_NormalMatrix * gl_Normal);
-	
-	gl_Position = projection_matrix * modelview_matrix * vec4(vertex, 1.0);
+{
+        Color = VertexColor;
+        gl_Position = vec4(VertexPosition,1.0);
 }
