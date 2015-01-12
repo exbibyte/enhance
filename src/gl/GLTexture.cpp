e581ef25147b96781fdc0f5c9d810984fc2fbd15
Bill Liu <yl@dhcp-9c-f3-87-b2-cd-16.ubcvisitor.wifi.ubc.ca>
2015-01-11 19:40:01 -0800

    changed SetTextureRgba() to SetTextureRgb()

--- src/gl/GLTexture.cpp
+++ src/gl/GLTexture.cpp
@@ -1,8 +1,12 @@
 #include "GLTexture.h"
+#include "PPM.hpp"
 
 #include <GLFW/glfw3.h>
 
-GLTexture::GLTexture() : _Tex(-1), _Fbo(-1) {
+#include <string>
+using namespace std;
+
+GLTexture::GLTexture() : _Tex(-1), _Fbo(-1), _iActiveTexture(-1) {
     
 }
 
@@ -24,6 +28,27 @@ bool GLTexture::GetFboId( GLuint & FboId ) {
     }
 }
 
+bool GLTexture::GetActiveTexture( int & iActiveTexture ) {
+    if( _iActiveTexture >= 0 ) {
+        iActiveTexture = _iActiveTexture;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool GLTexture::SetTextureFromFile( string strTexturePath, int iActiveTexture )
+{
+    bool bRet = true;
+
+    int width, height;
+    GLubyte * ImgData = PPM::Read( strTexturePath, width, height );
+    bRet = SetTexture( RGB, width, height, ImgData, iActiveTexture );
+    delete [] ImgData;
+    ImgData = 0;
+    return bRet;
+}
+
 bool GLTexture::SetTexture( eTextureType TextureType, int iWidth, int iHeight, void * const Data, int iActiveTexture ) {
     bool bRet = true;
 
@@ -40,9 +65,9 @@ bool GLTexture::SetTexture( eTextureType TextureType, int iWidth, int iHeight, v
         bRet = SetTextureDepth( iWidth, iHeight, iActiveTexture );
     }
     break;
-    case RGBA:
+    case RGB:
     {
-        bRet = SetTextureRgba( iWidth, iHeight, Data, iActiveTexture );
+        bRet = SetTextureRgb( iWidth, iHeight, Data, iActiveTexture );
     }
     break;
     default:
@@ -50,8 +75,27 @@ bool GLTexture::SetTexture( eTextureType TextureType, int iWidth, int iHeight, v
     }
     return bRet;
 }
-bool GLTexture::SetTextureRgba( int iWidth, int iHeight, void * const Data, int iActiveTexture ) {
+bool GLTexture::SetTextureRgb( int iWidth, int iHeight, void * const Data, int iActiveTexture ) {
     bool bRet = true;
+
+    glGenTextures( 1, &_Tex );
+    //Assign the shadow map to designated texture channel
+    glActiveTexture( GL_TEXTURE0 + iActiveTexture );    
+    //bind texture to current texture holder
+    glBindTexture(GL_TEXTURE_2D, _Tex );
+
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
+
+    //pass texture to openGL
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, iWidth, iHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, Data );
+
+    //let opengl automatically build mipmap
+    glGenerateMipmap( GL_TEXTURE_2D );    
+
+    _iActiveTexture = iActiveTexture;
     
     return bRet;
 }
@@ -60,9 +104,13 @@ bool GLTexture::SetTextureDepth( int iWidth, int iHeight, int iActiveTexture ) {
     bool bRet = true;
 
     GLfloat border[]={ 1.0f,0.0f,0.0f,0.0f };
+
     //The shadow maptexture
     glGenTextures( 1, &_Tex );
+    //Assign the shadow map to designated texture channel
+    glActiveTexture( GL_TEXTURE0 + iActiveTexture );
     glBindTexture( GL_TEXTURE_2D, _Tex );
+    
     glTexImage2D( GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, iWidth, iHeight, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, NULL);
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
@@ -71,9 +119,7 @@ bool GLTexture::SetTextureDepth( int iWidth, int iHeight, int iActiveTexture ) {
     glTexParameterfv( GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, border );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LESS );
-    //Assign the shadow map to texture channel 0
-    glActiveTexture( GL_TEXTURE0 + iActiveTexture );
-    glBindTexture( GL_TEXTURE_2D, _Tex );
+    
     //Create and set up the FBO
     glGenFramebuffers( 1, &_Fbo );
     glBindFramebuffer( GL_FRAMEBUFFER, _Fbo );
@@ -83,14 +129,18 @@ bool GLTexture::SetTextureDepth( int iWidth, int iHeight, int iActiveTexture ) {
     // Revert to the default framebuffer for now
     glBindFramebuffer( GL_FRAMEBUFFER, 0 );
 
+    _iActiveTexture = iActiveTexture;
+    
     return bRet;
 }
 
 bool GLTexture::BindFbo() {
     glBindFramebuffer( GL_FRAMEBUFFER, _Fbo );
+    return true;
 }
 
 bool GLTexture::UnbindFbo() {
     // Revert to the default framebuffer for now
     glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+    return true;
 }
--- src/gl/GLTexture.h
+++ src/gl/GLTexture.h
@@ -11,23 +11,29 @@
 
 #include <GLFW/glfw3.h>
 
+#include <string>
+using namespace std;
+
 class GLTexture {
 public:
     enum eTextureType {
         DEPTH,
-        RGBA,
+        RGB,
     };
     GLTexture();
     bool BindFbo();
     bool UnbindFbo();
     bool GetTextureId( GLuint & TextureId );
     bool GetFboId( GLuint & FboId );
+    bool GetActiveTexture( int & iActiveTexture );
+    bool SetTextureFromFile( string strTexturePath, int iActiveTexture );
     bool SetTexture( eTextureType TextureType, int iWidth, int iHeight, void * const Data, int iActiveTexture );
 private:
-    bool SetTextureRgba( int iWidth, int iHeight, void * const Data, int iActiveTexture );
+    bool SetTextureRgb( int iWidth, int iHeight, void * const Data, int iActiveTexture );
     bool SetTextureDepth( int iWidth, int iHeight, int iActiveTexture );
     GLuint _Tex;
     GLuint _Fbo;
+    int _iActiveTexture;
 };
 
 #endif
