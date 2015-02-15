b1d486416be654e79d75f31dace90d924e3bdf61
Bill <yl@Bills-MacBook-Air.local>
2015-02-14 20:53:57 -0800

    added unconnected vertex test

--- test/polymesh/test_VV.cpp
+++ test/polymesh/test_VV.cpp
@@ -90,3 +90,48 @@ TEST_CASE( "VV", "[VV]" ) {
     CHECK( MapVec.size() == 5 );
   }
 }
+TEST_CASE( "VV_Unconnected", "[VV_Uncon]" ) {
+  SECTION( "Check unconnected vertices" ){
+    vector< Vec > vVertex;
+    vector< set< int > > vConnect;
+    for( int i = 0; i < 4; i++ ){ 
+      Vec a = Vec(3);
+      a._vec[0] = 5;
+      a._vec[1] = 5;
+      a._vec[2] = 5;
+      vVertex.push_back( a );
+    }
+    
+    int conn0[] = {1,2};
+    int conn1[] = {0,2};
+    int conn2[] = {0,1,3};
+    int conn3[] = {2};
+    set<int> con0 (conn0, conn0+2);
+    set<int> con1 (conn1, conn1+2);
+    set<int> con2 (conn2, conn2+3);
+    set<int> con3 (conn3, conn3+1);
+    vConnect.push_back( con0 );
+    vConnect.push_back( con1 );
+    vConnect.push_back( con2 );
+    vConnect.push_back( con3 );
+    
+    PolyMesh_VV MeshVV;
+    MeshVV.SetVertices( vVertex, vConnect );
+
+    map< int, set<int> > MapFaceList;
+    map< int, set<int> > MapVertexList;
+    map< int, Vec > MapVec;
+    bool bRet = MeshVV.CalcFaces( MapFaceList, MapVertexList, MapVec );
+    CHECK( bRet == false );
+    REQUIRE( MapFaceList.size() == 1 );
+
+    CHECK( MapFaceList[0].find(0) != MapFaceList[0].end() );
+    CHECK( MapFaceList[0].find(1) != MapFaceList[0].end() );
+    CHECK( MapFaceList[0].find(2) != MapFaceList[0].end() );
+    CHECK( MapFaceList[0].find(3) == MapFaceList[0].end() );
+
+    CHECK( MapVertexList.size() == 3 );
+    
+    CHECK( MapVec.size() == 4 );
+  }
+}
