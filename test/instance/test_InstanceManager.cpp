6fe774a1d053173bce27208bb3aab9b5f5675ee7
Bill Liu <yuan6liu@gmail.com>
2015-12-19 21:06:23 -0800

    Added support for basic data types in InstanceManager

--- test/instance/test_InstanceManager.cpp
+++ test/instance/test_InstanceManager.cpp
@@ -22,7 +22,7 @@
 using namespace std;
 
 TEST_CASE( "InstanceManager", "[ALL]" ) {
-    SECTION( "InstanceManager" ){
+    SECTION( "InstanceManager Complex Types" ){
         InstanceManager< InstanceType::ObjectGeneric, InstanceType::Player, InstanceType::Spectator > _manager_generic;
 	int val_set1 = 5;
 	int val_set2 = 10;
@@ -50,5 +50,51 @@ TEST_CASE( "InstanceManager", "[ALL]" ) {
 	bRet &= _manager_generic.QueryValue<InstanceType::Spectator>( 1, val_query4 );
 	CHECK( true == bRet );
 	CHECK( 20 == val_query4 );
+    }
+    SECTION( "InstanceManager Basic Types" ){
+        InstanceManager< int > _manager_int;
+	int val_set1 = 5;
+	int val_set2 = 10;
+	bool bRet = _manager_int.SetValue( 1, val_set1 );
+	bRet &= _manager_int.SetValue( 2, val_set2 );
+	int val_query1;
+	int val_query2;
+	bRet &=  _manager_int.QueryValue( 1, val_query1 );
+	bRet &=  _manager_int.QueryValue( 2, val_query2 );
+	CHECK( true == bRet );
+	CHECK( 5 == val_query1 );
+	CHECK( 10 == val_query2 );
+
+	bRet = _manager_int.SetValue( 1, val_set2 );
+	bRet &=  _manager_int.QueryValue( 1, val_query1 );
+	CHECK( true == bRet );
+	CHECK( 10 == val_query1 );
+    }
+    SECTION( "InstanceManager Basic Vectors" ){
+        InstanceManager< vector<double> > _manager_vec_double;
+	vector<double> set_vec_1 = std::initializer_list<double>{ 6.6, 7.7, 8.8 };
+	vector<double> set_vec_2 = std::initializer_list<double>{ 1.1, 2.2, 3.3 };
+	bool bRet = _manager_vec_double.SetValue( 1, set_vec_1 );
+	bRet &= _manager_vec_double.SetValue( 2, set_vec_2 );
+
+        vector<double> query_vec_1;
+	vector<double> query_vec_2;
+	bRet &=  _manager_vec_double.QueryValue( 1, query_vec_1 );
+	bRet &=  _manager_vec_double.QueryValue( 2, query_vec_2 );
+
+	CHECK( true == bRet );
+	double i = query_vec_1[0];
+	CHECK( 6.6 == i );
+	i = query_vec_1[1];
+	CHECK( 7.7 == i );
+	i = query_vec_1[2];
+	CHECK( 8.8 == i );
+
+	i = query_vec_2[0];
+	CHECK( 1.1 == i );
+	i = query_vec_2[1];
+	CHECK( 2.2 == i );
+	i = query_vec_2[2];
+	CHECK( 3.3 == i );
     }
 }
