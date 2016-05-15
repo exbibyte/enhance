0fa8672c632782c3eb5e75815ba026593684dfce
Bill Liu <yuan6liu@gmail.com>
2016-05-15 21:47:04 +0900

    Added static _component_type member as enComponentType::RENDER.

--- src/enCode/enRenderPass.h
+++ src/enCode/enRenderPass.h
@@ -4,7 +4,13 @@
 #include "RenderBackend.h"
 #include "PassType_ShadowMap_OpGL.h"
 #include "RenderPass.h"
+#include "enComponentType.h"
 
-class enRenderPass_ShadowMap_OpGL : public RenderPass< RenderBackEndOpenGL, PassType_ShadowMap_OpGL > {};
+class enRenderPass_ShadowMap_OpGL : public RenderPass< RenderBackEndOpenGL, PassType_ShadowMap_OpGL > {
+public:
+    static enComponentType _component_type;
+};
+
+enComponentType enRenderPass_ShadowMap_OpGL::_component_type = enComponentType::RENDER;
 
 #endif
