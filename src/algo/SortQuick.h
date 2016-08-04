985639e89860f4df959161feb47405c483feae4c
unknown <biliu@CARMD-EL-001668.sierrawireless.local>
2016-08-04 11:28:03 -0700

    Added an extra partition for equal range. Added random selection for pivot.

--- src/algo/SortQuick.h
+++ src/algo/SortQuick.h
@@ -2,34 +2,48 @@
 #define SORT_QUICK_H
 
 #include <vector>
+#include <utility>
+#include <chrono>
+#include <random>
 
 template< class TypeVal >
 class SortQuick {
 public:
+    //default non-descending order comparator with < operator
     class CompDefault {
     public:
 	bool operator()( TypeVal const & a, TypeVal const & b ){
 	    return a < b;
 	}
     };
+    //default non-ascending order comparator with > operator
     class CompDefault_Desc {
     public:
 	bool operator()( TypeVal const & a, TypeVal const & b ){
-	    return a >= b;
+	    return a > b;
 	}
     };
     template< class Comp = CompDefault >
     static void Sort( std::vector< TypeVal > & values, int i_start, int i_end, Comp comparator = Comp() ){
 	if( i_end - i_start <= 0 ) return; //ends if only single element
-	//partition
-	int i_middle = Partition( values, i_start, i_end, comparator );
+	//partition into 3 ranges: [less, equal, greater]
+        std::pair<int,int> i_middle_range = Partition( values, i_start, i_end, comparator );
 	//sort lower partition
-	Sort( values, i_start, i_middle - 1, comparator );
+	Sort( values, i_start, i_middle_range.first - 1, comparator );
 	//sort higher partition
-	Sort( values, i_middle + 1, i_end, comparator );
+	Sort( values, i_middle_range.second + 1, i_end, comparator );
     }
     template< class Comp >
-    static int Partition( std::vector< TypeVal > & values, int i_start, int i_end, Comp comparator ){
+    static std::pair<int,int> Partition( std::vector< TypeVal > & values, int i_start, int i_end, Comp comparator ){
+        //select a random index for pivot point
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::default_random_engine generator(seed);
+        std::uniform_int_distribution<int> distribution( i_start, i_end );
+        int random_index = distribution( generator );
+        //temporary put pivot to the end
+        std::swap( values[ random_index ], values[ i_end ] );
+
+        //partition into less and greater sections
 	TypeVal middle = values[i_end];
 	int i_start_of_higher = i_start;
 	for( int i = i_start; i <= i_end - 1; ++i ){
@@ -44,7 +58,23 @@ public:
 	values[ i_end ] = values[ i_start_of_higher ];
 	values[ i_start_of_higher ] = middle;
 	int i_middle = i_start_of_higher;
-	return i_middle;
+        //parition range of equal middle elements
+        int i_middle_start = i_middle;
+        while( i_middle_start-1 >= i_start ){
+            //continue until an alement less than middle is reached
+            if( comparator( values[ i_middle_start-1 ], middle ) ){
+                break;
+            }
+            i_middle_start--;
+        }
+        while( i_middle+1 >= i_end ){
+            //continue until an alement greater than middle is reached
+            if( comparator( middle, values[ i_middle+1 ] ) ){
+                break;
+            }
+            i_middle++;
+        }
+	return std::pair<int,int>( i_middle_startm, i_middle );
     }
 };
 
