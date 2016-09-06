0d97f5d41acc7b22412b2f221a4f83776a24b9c0
BIll Liu <yuan6liu@gmail.com>
2016-09-06 00:04:51 -0700

    Modified interface.

--- src/core/IQueue.hpp
+++ src/core/IQueue.hpp
@@ -5,7 +5,7 @@ template< class T >
 class IQueue {
 public:
     using ItemType = T;
-    virtual void enqeue( ItemType & item ){}
+    virtual bool enqeue( ItemType & item ){ return false; }
     virtual bool dequeue( ItemType & item ){ return false; }
     virtual size_t size(){ return 0; }
     virtual ~IQueue(){}
