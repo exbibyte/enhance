251ec44e7024f90586ccff85caa67bafc2cec947
BIll Liu <yuan6liu@gmail.com>
2017-02-05 03:37:36 -0800

    Modified radix-2 dit fft with negative sign convention.

--- src/math/fft.cpp
+++ src/math/fft.cpp
@@ -1,6 +1,7 @@
 #include "fft.hpp"
 
 #include <vector>
+#include <complex>
 
 using namespace std;
 
@@ -14,18 +15,46 @@ void fft::pad_to_nearest_power_of_2( vector<double> & input ){
     input.insert( input.begin(), pad, 0 );
 }
 
-void fft::fft_cooley_tukey_radix_2( vector<double> & input, vector<double> & output ){
-    
+vector<complex<double> > fft::fft_cooley_tukey_radix_2_dit( vector<complex<double> > & input ){
+    //performs radix-2 decimation in frequency algorithm
+    //w = e^(2*pi*i/n)
+    int n = input.size();
+    if( 1 == n ){
+	return input;
+    }
+    complex<double> i(0,1);
+    complex<double> exponent = -2.0 * fft::PI * i / (double)n;
+    complex<double> w_base = std::exp(exponent);
+    complex<double> w(1,0);
+    vector<complex<double> > y_o(n/2);
+    vector<complex<double> > y_e(n/2);
+    for( int i = 0; i < n/2; ++i ){
+	y_e[i] = input[2*i];
+	y_o[i] = input[2*i+1];
+    }
+    vector<complex<double> > y_o_ret = fft_cooley_tukey_radix_2_dit( y_o );
+    vector<complex<double> > y_e_ret = fft_cooley_tukey_radix_2_dit( y_e );
+    vector<complex<double> > y(n);
+    for( int i = 0; i < n/2; ++i ){
+	y[i] = y_e_ret[i] + w * y_o_ret[i];
+	y[i + n/2] = y_e_ret[i] - w * y_o_ret[i];
+	w = w * w_base;
+    }
+    return y;
 }
 
 void fft::ifft_cooley_tukey_radix_2( vector<double> & input, vector<double> & output ){
     
 }
 
-void fft::fft_cooley_tukey( vector<double> & input, vector<double> & output ){
+void fft::fft_cooley_tukey( vector<double> & input, vector<complex<double> > & output ){
     //padd to nearest power of 2 length
     pad_to_nearest_power_of_2( input );
-    fft_cooley_tukey_radix_2( input, output );
+    vector<complex<double> > input_complex;
+    for( int i = 0; i < input.size(); ++i ){
+	input_complex.push_back(input[i]);
+    }
+    output = fft_cooley_tukey_radix_2_dit( input_complex );
 }
 
 void fft::ifft_cooley_tukey( vector<double> & input, vector<double> & output ){
--- src/math/fft.hpp
+++ src/math/fft.hpp
@@ -2,12 +2,17 @@
 #define FFT_HPP
 
 #include <vector>
+#include <complex>
 
 class fft {
 public:
-    static void fft_cooley_tukey( std::vector<double> & input, std::vector<double> & output );
+    static constexpr double PI = 3.14159265359;
+
+    static void fft_cooley_tukey( std::vector<double> & input, std::vector<std::complex<double> > & output );
     static void ifft_cooley_tukey( std::vector<double> & input, std::vector<double> & output );
-    static void fft_cooley_tukey_radix_2( std::vector<double> & input, std::vector<double> & output );
+
+    //helpers
+    static std::vector<std::complex<double> > fft_cooley_tukey_radix_2_dit( std::vector<std::complex<double> > & input );
     static void ifft_cooley_tukey_radix_2( std::vector<double> & input, std::vector<double> & output );
     static void pad_to_nearest_power_of_2( std::vector<double> & input );
     static unsigned int reverse_bits( unsigned int input, unsigned int num_bits );
