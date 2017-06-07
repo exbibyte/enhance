e375313e2e34551efb458737d2c75325682090d1
Bill Liu <yuan6liu@gmail.com>
2017-06-06 23:48:09 -0700

    added further dependencies in install script.

---
+++ install_dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "installing dependencies..."
+
+sudo apt install -y pkg-config xorg-dev libgl1-mesa-dev
+
+echo "downloading and installing http://github.com/skaslev/gl3w"
+git clone http://github.com/skaslev/gl3w.git
+mkdir temp && gl3w/gl3w_gen.py temp
+cp temp/include/GL/* core/impl/render/gl/GL/
+cp temp/src/* core/impl/render/gl/GL/
+rm -r -f temp gl3w
+
+echo "downloading and installing http://github.com/glfw/glfw"
+git clone http://github.com/glfw/glfw.git
+cd glfw && cmake CMakeLists.txt && make && make install && cd ../ && rm -r -f glfw
+
+echo "finished."
