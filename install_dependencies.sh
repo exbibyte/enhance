346243f232f4d1287b939565a4035c97e71153b5
Bill Liu <yuan6liu@gmail.com>
2017-06-06 23:54:30 -0700

    modified install script.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,6 +7,7 @@ sudo apt install -y pkg-config xorg-dev libgl1-mesa-dev
 echo "downloading and installing http://github.com/skaslev/gl3w"
 git clone http://github.com/skaslev/gl3w.git
 mkdir temp && gl3w/gl3w_gen.py temp
+mkdir core/impl/render/gl/GL
 cp temp/include/GL/* core/impl/render/gl/GL/
 cp temp/src/* core/impl/render/gl/GL/
 rm -r -f temp gl3w
