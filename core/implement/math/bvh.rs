447f30b5f331623f100e8e2a99bf3cc5718e3da0
Bill Liu <yuan6liu@gmail.com>
2017-09-16 17:54:10 -0700

    added surface area heuristic to bvh.

--- core/implement/math/bvh.rs
+++ core/implement/math/bvh.rs
@@ -2,10 +2,12 @@ use std::boxed::Box;
 use std::f64;
 use std::u64;
 
-use interface::i_spatial_accel;
+use interface::i_spatial_accel::ISpatialAccel;
 use interface::i_bound::{ IBound, BoundType };
+use interface::i_stat_tree::IStatTree;
 
 use implement::math::bound::AxisAlignedBBox;
+use implement::math::bound::Axis;
 
 /// implementation of spatial acceleration using bounding volume hierarchy with surface area heuristic
 pub struct Bvh {
@@ -68,7 +70,7 @@ impl NodeBvh {
         let mut u : AxisAlignedBBox = Default::default();
         u.get_union( &b[..] );
 
-        println!( "bound union: {:?}", u );
+        // println!( "bound union: {:?}", u );
 
         //check for leaf condition
         if objs.len() == 1 {
@@ -78,79 +80,102 @@ impl NodeBvh {
         }
 
         //todo: execute alternative branch when number of objects are low
-        //todo: surface area heuristic
         
         //get the longest axis of the bounding box
         let ( ref axis, ref length ) = u.get_longest_axis();
 
         self._bound = u;
 
-        println!( "axis length: {}", length );
+        // println!( "axis length: {}", length );
         
         let c = self._bound.get_centroid();
         let bin_len = length / num_bins as f64;
 
         let mut bin_offsets = vec![];
         for i in 0..num_bins {
-            match axis {
-                X => bin_offsets.push( i as f64 * bin_len + self._bound._bound_lower[0] ),
-                Y => bin_offsets.push( i as f64 * bin_len + self._bound._bound_lower[1] ),
-                Z => bin_offsets.push( i as f64 * bin_len + self._bound._bound_lower[2] ),
+            match *axis {
+                Axis::X => { bin_offsets.push( i as f64 * bin_len + self._bound._bound_lower[0] ); },
+                Axis::Y => { bin_offsets.push( i as f64 * bin_len + self._bound._bound_lower[1] ); },
+                Axis::Z => { bin_offsets.push( i as f64 * bin_len + self._bound._bound_lower[2] ); },
             }
         }
 
-        //bin objects using their centroids in the direction of the longest axis
-        let mut bins = vec![ 0; num_bins as usize ];
+        //bin objects using use surface area heuristics with their entry and exit points in the long axis of bounding box
+        //determine median and bin objects with their centroids
+        let mut bins_surf_area = vec![ 0i64; num_bins as usize ];
         let mut obj_bin = vec![];
         let epsilon = 0.001f64;
         for i in objs {
             let c = i.1.get_centroid();
-            println!("centroid: {:?}", c );
-            let bin_id = match axis {
-                X => {
+            let obj_bound_data = i.1.get_bound_data();
+            let obj_entry = &obj_bound_data[0..3];
+            let obj_exit = &obj_bound_data[3..6];
+            
+            // println!("centroid: {:?}", c );
+            let bin_id = match *axis {
+                Axis::X => {
+                    let entry_bin = ( ( obj_entry[0] - self._bound._bound_lower[0] ) / length * (num_bins as f64) * ( 1.0 - epsilon ) ) as usize;
+                    let exit_bin = ( ( obj_exit[0] - self._bound._bound_lower[0] ) / length * (num_bins as f64) * ( 1.0 - epsilon ) ) as usize;
+                    bins_surf_area[ entry_bin ] += 1;
+                    bins_surf_area[ exit_bin ] -= 1;
+                        
                     ( ( c[0] - self._bound._bound_lower[0] ) / length * (num_bins as f64) * ( 1.0 - epsilon ) ) as usize
                 },
-                Y => {
+                Axis::Y => {
+                    let entry_bin = ( ( obj_entry[1] - self._bound._bound_lower[1] ) / length * (num_bins as f64) * ( 1.0 - epsilon ) ) as usize;
+                    let exit_bin = ( ( obj_exit[1] - self._bound._bound_lower[1] ) / length * (num_bins as f64) * ( 1.0 - epsilon ) ) as usize;
+                    bins_surf_area[ entry_bin ] += 1;
+                    bins_surf_area[ exit_bin ] -= 1;
+
                     ( ( c[1] - self._bound._bound_lower[1] ) / length * (num_bins as f64) * ( 1.0 - epsilon ) ) as usize
                 },
-                Z => {
+                Axis::Z => {
+                    let entry_bin = ( ( obj_entry[2] - self._bound._bound_lower[2] ) / length * (num_bins as f64) * ( 1.0 - epsilon ) ) as usize;
+                    let exit_bin = ( ( obj_exit[2] - self._bound._bound_lower[2] ) / length * (num_bins as f64) * ( 1.0 - epsilon ) ) as usize;
+                    bins_surf_area[ entry_bin ] += 1;
+                    bins_surf_area[ exit_bin ] -= 1;
+
                     ( ( c[2] - self._bound._bound_lower[2] ) / length * (num_bins as f64) * ( 1.0 - epsilon ) ) as usize
                 },
             };
-            if bin_id >= ( num_bins as usize ) {
-                println!("bin_id: {}, bin_len: {}", bin_id, num_bins );
-                assert!( bin_id < ( num_bins as usize ) );
-            }
-            bins[bin_id] += 1;
             obj_bin.push( bin_id );
         }
 
-        println!( "bins: {:?}", bins );
+        // println!( "bins surf area: {:?}", bins_surf_area );
 
-        let sum = bins.iter_mut().fold( 0, | acc, x | {
-            let s = acc + *x;
+        let _ = bins_surf_area.iter_mut().fold( 0, | acc, x | {
             *x += acc;
-            s
+            *x
         }
         );
-        let median = sum / 2;
 
-        let mut split_bin_idx = 0;
-        for (idx, i) in bins.iter().enumerate() {
-            if *i > median {
-                break;
-            }
-            split_bin_idx += 1;
+        let mut sum_surf_area = 0;
+        for i in bins_surf_area.iter() {
+            sum_surf_area += *i;
         }
 
-        println!( "split_bin_idx: {}", split_bin_idx );
+        let median_surf_area = sum_surf_area / 2;
+        // println!( "bins surf area: {:?}, sum_surf_area: {}", bins_surf_area, sum_surf_area );
+        // println!( "median surf area: {:?}", median_surf_area );
+
+        let mut split_bin_idx = 0;
+        let mut accum_surf_area = 0;
+        for (idx, i) in bins_surf_area.iter().enumerate() {
+            accum_surf_area += *i;
+            if accum_surf_area > median_surf_area {
+                split_bin_idx = idx;
+                break;
+            }
+        }
+
+        // println!( "split_bin_idx: {}", split_bin_idx );
 
         let mut bin_left : Vec< ( u64, &IBound ) > = vec![];
         let mut bin_right : Vec< ( u64, &IBound ) > = vec![];
         let mut idx_obj = 0;
         for i in obj_bin {
             let obj = objs[ idx_obj ];
-            if i < split_bin_idx {
+            if i <= split_bin_idx {
                 bin_left.push( obj );
             } else {
                 bin_right.push( obj );
@@ -160,8 +185,8 @@ impl NodeBvh {
 
         if bin_left.len() > 0 {
             let mut l : NodeBvh = Default::default();
-            println!("num left children: {}", bin_left.len() );
-            l.build_node( num_bins, &bin_left[..] );
+            // println!("num left children: {}", bin_left.len() );
+            l.build_node( num_bins, &bin_left[..] )?;
             self._left = BvhBranch::CHILD( Box::new( l ) );
         } else {
             self._left = BvhBranch::EMPTY;
@@ -169,8 +194,8 @@ impl NodeBvh {
 
         if bin_right.len() > 0 {
             let mut r : NodeBvh = Default::default();
-            println!("num right children: {}", bin_right.len() );
-            r.build_node( num_bins, &bin_right[..] );
+            // println!("num right children: {}", bin_right.len() );
+            r.build_node( num_bins, &bin_right[..] )?;
             self._right = BvhBranch::CHILD( Box::new( r ) );
         } else {
             self._right = BvhBranch::EMPTY;
@@ -180,7 +205,7 @@ impl NodeBvh {
     }
 }
 
-impl i_spatial_accel::ISpatialAccel for Bvh {
+impl ISpatialAccel for Bvh {
     fn query( & self, input: &IBound ) -> Vec< u64 >
     {
         unimplemented!();
@@ -210,5 +235,26 @@ impl Bvh {
             },
             _bins: bins,
         }
+    }
+}
+
+impl IStatTree for Bvh {
+    fn sum_subtree_child_count( & self ) -> Option< u64 > {
+        unimplemented!();
+    }
+    fn node_degree( & self ) -> Option< u64 > {
+        unimplemented!();
+    }
+    fn max_depth( & self ) -> Option< u64 > {
+        unimplemented!();
+    }
+    fn mean_depth( & self ) -> Option< u64 > {
+        unimplemented!();
+    }
+    fn min_depth( & self ) -> Option< u64 > {
+        unimplemented!();
+    }
+    fn balance_ratio( & self ) -> Option< f64 > {
+        unimplemented!();
     }
 }
