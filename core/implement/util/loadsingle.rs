c40bb4f5b31724fb6baf233955356074407c9ffd
Bill Liu <yuan6liu@gmail.com>
2018-01-27 15:48:08 -0800

    updated travis file.

--- core/implement/util/loadsingle.rs
+++ core/implement/util/loadsingle.rs
@@ -3,12 +3,15 @@ use interface::i_load::ILoad;
 #[derive(Clone, Debug)]
 pub struct LoadSingle < T > {
     _buf: Option< T >,
+    ///if true, no-load if there exists data
+    pub _is_blocking: bool,
 }
 
 impl < T > Default for LoadSingle < T > {
     fn default() -> Self {
         Self {
             _buf: None,
+            _is_blocking: false,
         }
     }
 }
@@ -16,13 +19,21 @@ impl < T > Default for LoadSingle < T > {
 impl < T > LoadSingle < T > {
 
     pub fn load( & mut self, t: T ) -> Result< (), & 'static str > {
-        self._buf = Some( t );
+        if self._is_blocking {
+            if self._buf.is_none() {
+                self._buf = Some( t );
+            }
+        } else {
+            self._buf = Some( t );
+        }
+        
         Ok( () )
     }
 
     pub fn apply< F >( & mut self, mut f: F ) -> Result< (), & 'static str >
         where F: FnMut( & T ) -> bool
     {
+
         let consumed = match self._buf {
             None => { false }, //do nothing if no data
             Some(ref x) => { f( x ); true },
@@ -31,7 +42,7 @@ impl < T > LoadSingle < T > {
         if consumed {
             self._buf = None;
         }
-        
+
         Ok( () )
     }
 }
